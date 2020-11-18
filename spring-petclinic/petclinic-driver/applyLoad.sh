48228d382951f1182b59538b49b3dd60f45f7100
Shruthi <35678265+shruacha1234@users.noreply.github.com>
2020-11-18 18:07:12 +0530

    Delete applyLoad.sh

--- spring-petclinic/petclinic-driver/applyLoad.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd $JMETER_HOME
-
-echo jmeter -n -t petclinic.jmx -j /output/petclinic.stats -JPETCLINIC_HOST=$JHOST -JPETCLINIC_PORT=$JPORT -Jduration=$JDURATION -Jusers=$JUSERS
-exec jmeter -n -t petclinic.jmx -j /output/petclinic.stats -JPETCLINIC_HOST=$JHOST -JPETCLINIC_PORT=$JPORT -Jduration=$JDURATION -Jusers=$JUSERS
