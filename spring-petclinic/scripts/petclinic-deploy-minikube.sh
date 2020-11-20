2a6bd50688ded16bad7d97e8206a102094e351c8
Shruthi <35678265+shruacha1234@users.noreply.github.com>
2020-11-20 15:40:16 +0530

    Added descriptions to functions

--- spring-petclinic/scripts/petclinic-deploy-minikube.sh
+++ spring-petclinic/scripts/petclinic-deploy-minikube.sh
@@ -1,19 +1,28 @@
-#!/bin/bash
-
 ROOT_DIR=.
 pushd ${ROOT_DIR}
 # Run the benchmark as
-# SCRIPT  MANIFESTS_DIR 
-# Ex of ARGS :   rt-cloud-benchmarks/spring-petclinic/manifests 
+# SCRIPT BENCHMARK_SERVER NAMESPACE MANIFESTS_DIR RESULTS_DIR_PATH
+# Ex of ARGS :  wobbled.os.fyre.ibm.com openshift-monitoring rt-cloud-benchmarks/spring-petclinic/manifests /petclinic/results
 
-LOGFILE="${ROOT_DIR}/setup.log"
-MANIFESTS_DIR=$1
+BENCHMARK_SERVER=$1
+NAMESPACE=$2
+MANIFESTS_DIR=$3
+SERVER_INSTANCES=$4
 
-if [[  -z $MANIFESTS_DIR ]]; then
-	echo "Do set the variable -   MANIFESTS_DIR "
+if [[ -z $BENCHMARK_SERVER || -z $NAMESPACE  || -z $MANIFESTS_DIR ]]; then
+	echo "Do set all the variables - BENCHMARK_SERVER , NAMESPACE and MANIFESTS_DIR "
 	exit 1
 fi
 
+if [ -z "${SERVER_INSTANCES}" ]; then
+	SERVER_INSTANCES=1
+else
+	SERVER_INSTANCES=$4
+fi
+
+# checks if the previous command is executed successfully
+# input:Return value of previous command
+# output:Prompts the error message if the return value is not zero
 function err_exit() {
 	if [ $? != 0 ]; then
 		printf "$*"
@@ -22,33 +31,69 @@ function err_exit() {
 	fi
 }
 
+# Create multiple yamls based on instances and Update the template yamls with names and create multiple files
+# input:petclinic and service-monitor yaml file
 function createInstances() {
-	# Deploy service monitor to get Java Heap recommendations from petclinic$
-	kubectl apply -f $MANIFESTS_DIR/service-monitor.yaml
-
 	#Create the deployments and services
-	kubectl apply -f $MANIFESTS_DIR/petclinic.yaml 
-	err_exit "Error: Issue in deploying."
+	#Using inmem DB so no DB specific pods
+
+	for(( inst=0; inst<${SERVER_INSTANCES}; inst++ ))
+	do
+		sed 's/petclinic/petclinic-'$inst'/g' $MANIFESTS_DIR/service-monitor.yaml > $MANIFESTS_DIR/service-monitor-$inst.yaml
+		sed -i 's/petclinic-app/petclinic-app-'$inst'/g' $MANIFESTS_DIR/service-monitor-$inst.yaml
+		sed -i 's/petclinic-port/petclinic-port-'$inst'/g' $MANIFESTS_DIR/service-monitor-$inst.yaml
+		oc create -f $MANIFESTS_DIR/service-monitor-$inst.yaml -n $NAMESPACE
+	done
+	port=32334
+	for(( inst=0; inst<${SERVER_INSTANCES}; inst++ ))
+	do
+		sed 's/petclinic-sample/petclinic-sample-'$inst'/g' $MANIFESTS_DIR/petclinic.yaml > $MANIFESTS_DIR/petclinic-$inst.yaml
+		sed -i 's/petclinic-service/petclinic-service-'$inst'/g' $MANIFESTS_DIR/petclinic-$inst.yaml
+		sed -i 's/petclinic-app/petclinic-app-'$inst'/g' $MANIFESTS_DIR/petclinic-$inst.yaml
+		sed -i 's/petclinic-port/petclinic-port-'$inst'/g' $MANIFESTS_DIR/petclinic-$inst.yaml
+		sed -i 's/32334/'$port'/g' $MANIFESTS_DIR/petclinic-$inst.yaml
+		oc create -f $MANIFESTS_DIR/petclinic-$inst.yaml -n $NAMESPACE
+		err_exit "Error: Issue in deploying."
+		((port=port+1))
+	done
 
 	#Wait till petclinic starts
 	sleep 40
+	#Expose the services
+	svc_list=($(oc get svc --namespace=$NAMESPACE | grep "service" | grep "petclinic" | cut -d " " -f1))
+	for sv in "${svc_list[@]}"
+	do
+		oc expose svc/$sv --namespace=$NAMESPACE
+		err_exit " Error: Issue in exposing service"
+	done
 }
 
+# Delete the petclinic deployments,services and routes if it is already present 
 function stopAllInstances() {
 	# Delete the deployments first to avoid creating replica pods
-	petclinic_deployments=($(kubectl get deployments  | grep "petclinic" | cut -d " " -f1))
-	
-	for de in "${petclinic_deployments[@]}"	
+	petclinic_deployments=($(oc get deployments --namespace=$NAMESPACE | grep "petclinic" | cut -d " " -f1))
+
+	for de in "${petclinic_deployments[@]}"
 	do
-		kubectl delete deployment $de 
+		oc delete deployment $de --namespace=$NAMESPACE
 	done
 
 	#Delete the services and routes if any
-	petclinic_services=($(kubectl get services  | grep "petclinic" | cut -d " " -f1))
+	petclinic_services=($(oc get services --namespace=$NAMESPACE | grep "petclinic" | cut -d " " -f1))
 	for se in "${petclinic_services[@]}"
 	do
-		kubectl delete service $se 
-	done	
+		oc delete service $se --namespace=$NAMESPACE
+	done
+	petclinic_routes=($(oc get route --namespace=$NAMESPACE | grep "petclinic" | cut -d " " -f1))
+	for ro in "${petclinic_routes[@]}"
+	do
+		oc delete route $ro --namespace=$NAMESPACE
+	done
+	petclinic_service_monitors=($(oc get servicemonitor --namespace=$NAMESPACE | grep "petclinic" | cut -d " " -f1))
+	for sm in "${petclinic_service_monitors[@]}"
+	do
+		oc delete servicemonitor $sm --namespace=$NAMESPACE
+	done
 }
 
 # Stop all petclinic related instances if there are any
