c7329224514b05e24778d2dafc76d9448181e013
kusumachalasani <kchalasa@redhat.com>
2022-02-02 20:00:22 +0530

    add JAVA_OPTIONS env in yaml

--- techempower/scripts/tfb-qrh-deploy-openshift.sh
+++ techempower/scripts/tfb-qrh-deploy-openshift.sh
@@ -299,8 +299,7 @@ function createInstances() {
 
                 done
 
-                sed -i "/env:/a \ \ \ \ \ \ \ \ \ \ \ \ value: \"${OPTIONS_VAR}\" " ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
-                sed -i '/env:/a \ \ \ \ \ \ \ \ \ \ - name: "JAVA_OPTIONS"' ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
+		sed -i "s/\"-server\"/\"${OPTIONS_VAR}\" " ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml
 
 		oc create -f ${MANIFESTS_DIR}/quarkus-resteasy-hibernate-${inst}.yaml -n ${NAMESPACE}
 		#err_exit "Error: Issue in deploying tfb-qrh." >> ${LOGFILE}
