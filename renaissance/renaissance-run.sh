cc2329479a72015eeb74b3ec41821c77dbfeef21
Prakalp <prakalp.work@gmail.com>
2022-07-18 13:24:20 +0530

    Update renaissance-run.sh

--- renaissance/renaissance-run.sh
+++ renaissance/renaissance-run.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-### Script to perform load test on multiple instances of techempower Quarkus benchmarks on openshift###
+### Script to perform load test on multiple instances of renaissance benchmark on openshift/minikube###
 #
 CURRENT_DIR="$(dirname "$(realpath "$0")")"
 source ${CURRENT_DIR}/../renaissance-common.sh
@@ -42,7 +42,7 @@ function err_exit() {
 		cat ${RESULTS_DIR_ROOT}/app-calc-metrics-measure-raw.log
 		cat ${RESULTS_DIR_ROOT}/server_requests-metrics-${TYPE}-raw.log
 		## Cleanup all the deployments
-		${SCRIPT_REPO}/tfb-cleanup.sh -c ${CLUSTER_TYPE} -n ${NAMESPACE} >> ${LOGFILE}
+		${SCRIPT_REPO}/renaissance-cleanup.sh -c ${CLUSTER_TYPE} -n ${NAMESPACE} >> ${LOGFILE}
 		exit -1
 	fi
 }
@@ -226,7 +226,7 @@ do
 		RE_DEPLOY="true"
 		;;
 	g)
-		TFB_IMAGE="${OPTARG}"		
+		renaissance_IMAGE="${OPTARG}"		
 		;;
 	n)
 		NAMESPACE="${OPTARG}"		
@@ -267,8 +267,8 @@ if [ -z "${RE_DEPLOY}" ]; then
 	RE_DEPLOY=false
 fi
 
-if [ -z "${TFB_IMAGE}" ]; then
-	TFB_IMAGE="${TFB_DEFAULT_IMAGE}"
+if [ -z "${renaissance_IMAGE}" ]; then
+	renaissance_IMAGE="${TFB_DEFAULT_IMAGE}"
 fi
 
 if [ -z "${NAMESPACE}" ]; then
@@ -323,3 +323,139 @@ function check_app() {
                 fi
         done
 }
+# Download the required dependencies 
+# output: Check if the hyperfoil/wrk dependencies is already present, If not download the required dependencies to apply the load
+function load_setup(){
+	if [ ! -d "${SCRIPT_REPO}/hyperfoil-${HYPERFOIL_VERSION}" ]; then
+		wget https://github.com/Hyperfoil/Hyperfoil/releases/download/release-${HYPERFOIL_VERSION}/hyperfoil-${HYPERFOIL_VERSION}.zip >> ${LOGFILE} 2>&1
+		err_exit "Error: Could not download the dependencies" >> ${LOGFILE}
+		unzip -o hyperfoil-${HYPERFOIL_VERSION}.zip -d ${SCRIPT_REPO} >> ${LOGFILE}
+	fi
+}
+# Perform warmup and measure runs
+# input: number of runs(warmup|measure), result directory 
+# output: Cpu info, memory info, node info, wrk load for each runs(warmup|measure) in the form of jason files
+function runItr()
+{
+	TYPE=$1
+	RUNS=$2
+	RESULTS_DIR_L=$3
+	for (( run=0; run<"${RUNS}"; run++ ))
+	do
+		# Check if the application is running
+		check_app 
+		# Get CPU and MEM info through prometheus queries
+		${SCRIPT_REPO}/perf/getmetrics-promql.sh ${TYPE}-${run} ${CPU_MEM_DURATION} ${RESULTS_DIR_L} ${BENCHMARK_SERVER} ${APP_NAME} ${CLUSTER_TYPE} &
+		# Run the wrk workload
+		run_wrk_with_scaling ${TYPE} ${run} ${RESULTS_DIR_L}
+		# Sleep till the wrk load completes
+		sleep ${DURATION}
+		sleep 1
+	done
+}
+# get the kruize recommendation for tfb application
+# input: result directory
+# output: kruize recommendations for tfb
+function get_recommendations_from_kruize()
+{
+	if [[ ${CLUSTER_TYPE} == "openshift" ]]; then
+		TOKEN=`${K_EXEC} whoami --show-token`
+	fi
+	APP_LIST=($(${K_EXEC} get deployments --namespace=${NAMESPACE} | grep "${APP_NAME}" | cut -d " " -f1))
+	for app in "${APP_LIST[@]}"
+	do
+		curl --silent -k -H "Authorization: Bearer ${TOKEN}" http://kruize-openshift-monitoring.apps.${BENCHMARK_SERVER}/recommendations?application_name=${app} > ${RESULTS_DIR_I}/${app}-recommendations.log
+		err_exit "Error: could not generate the recommendations for ${APP_NAME}" >> ${LOGFILE}
+	done
+}
+function runIterations() {
+	SCALING=$1
+	TOTAL_ITR=$2
+	WARMUPS=$3
+	MEASURES=$4
+	RESULTS_DIR_R=$5
+	for (( itr=0; itr<"${TOTAL_ITR}"; itr++ ))
+	do
+		echo "***************************************" >> ${LOGFILE}
+		echo "Starting iteration ${itr}" >> ${LOGFILE}
+		echo "***************************************" >> ${LOGFILE}
+		if [ ${RE_DEPLOY} == "true" ]; then
+			echo "Deploying the application..." >> ${LOGFILE}
+			${SCRIPT_REPO}/renaissance-deploy.sh --clustertype=${CLUSTER_TYPE} -s ${BENCHMARK_SERVER} -n ${NAMESPACE} -i ${SCALING} -g ${TFB_IMAGE} --dbtype=${DB_TYPE} --dbhost=${DB_HOST} --cpureq=${CPU_REQ} --memreq=${MEM_REQ} --cpulim=${CPU_LIM} --memlim=${MEM_LIM} --envoptions="${ENV_OPTIONS}" --usertunables="${OPTIONS_VAR}" --quarkustpcorethreads=${quarkustpcorethreads} --quarkustpqueuesize=${quarkustpqueuesize} --quarkusdatasourcejdbcminsize=${quarkusdatasourcejdbcminsize} --quarkusdatasourcejdbcmaxsize=${quarkusdatasourcejdbcmaxsize} --FreqInlineSize=${FreqInlineSize} --MaxInlineLevel=${MaxInlineLevel} --MinInliningThreshold=${MinInliningThreshold} --CompileThreshold=${CompileThreshold} --CompileThresholdScaling=${CompileThresholdScaling} --ConcGCThreads=${ConcGCThreads} --InlineSmallCode=${InlineSmallCode} --LoopUnrollLimit=${LoopUnrollLimit} --LoopUnrollMin=${LoopUnrollMin} --MinSurvivorRatio=${MinSurvivorRatio} --NewRatio=${NewRatio} --TieredStopAtLevel=${TieredStopAtLevel} --TieredCompilation=${TieredCompilation} --AllowParallelDefineClass=${AllowParallelDefineClass} --AllowVectorizeOnDemand=${AllowVectorizeOnDemand} --AlwaysCompileLoopMethods=${AlwaysCompileLoopMethods} --AlwaysPreTouch=${AlwaysPreTouch} --AlwaysTenure=${AlwaysTenure} --BackgroundCompilation=${BackgroundCompilation} --DoEscapeAnalysis=${DoEscapeAnalysis} --UseInlineCaches=${UseInlineCaches} --UseLoopPredicate=${UseLoopPredicate} --UseStringDeduplication=${UseStringDeduplication} --UseSuperWord=${UseSuperWord} --UseTypeSpeculation=${UseTypeSpeculation} >> ${LOGFILE}
+			# err_exit "Error: ${APP_NAME} deployment failed" >> ${LOGFILE}
+		fi
+		# Add extra sleep time for the deployment to complete as few machines takes longer time.
+		sleep 180
+		
+		##Debug
+		#Extra sleep time
+		#sleep 600
+		
+		# Start the load
+		RESULTS_DIR_I=${RESULTS_DIR_R}/ITR-${itr}
+		echo "Running ${WARMUPS} warmups" >> ${LOGFILE}
+		# Perform warmup runs
+		runItr warmup ${WARMUPS} ${RESULTS_DIR_I}
+		echo "Running ${MEASURES} measures" >> ${LOGFILE}
+		# Perform measure runs
+		runItr measure ${MEASURES} ${RESULTS_DIR_I}
+		sleep 15
+		# get the kruize recommendation for renaissance application
+		# commenting for now as it is not required in all cases
+		#get_recommendations_from_kruize ${RESULTS_DIR_I}
+		echo "***************************************" >> ${LOGFILE}
+		echo "Completed iteration ${itr}..." >> ${LOGFILE}
+		echo "***************************************" >> ${LOGFILE}
+	done
+}
+echo "INSTANCES ,  THROUGHPUT_RATE_3m , RESPONSE_TIME_RATE_3m , MAX_RESPONSE_TIME , RESPONSE_TIME_50p , RESPONSE_TIME_95p , RESPONSE_TIME_97p , RESPONSE_TIME_99p , RESPONSE_TIME_99.9p , RESPONSE_TIME_99.99p , RESPONSE_TIME_99.999p , RESPONSE_TIME_100p , CPU_USAGE , MEM_USAGE , CPU_MIN , CPU_MAX , MEM_MIN , MEM_MAX , THRPT_PROM_CI , RSPTIME_PROM_CI" > ${RESULTS_DIR_ROOT}/Metrics-prom.log
+echo ", THROUGHPUT_WRK , RESPONSETIME_WRK , RESPONSETIME_MAX_WRK , RESPONSETIME_STDEV_WRK , WEB_ERRORS , THRPT_WRK_CI , RSPTIME_WRK_CI" > ${RESULTS_DIR_ROOT}/Metrics-wrk.log
+echo ", CPU_REQ , MEM_REQ , CPU_LIM , MEM_LIM , QRKS_TP_CORETHREADS , QRKS_TP_QUEUESIZE , QRKS_DS_JDBC_MINSIZE , QRKS_DS_JDBC_MAXSIZE , FreqInlineSize , MaxInlineLevel , MinInliningThreshold , CompileThreshold , CompileThresholdScaling , ConcGCThreads , InlineSmallCode , LoopUnrollLimit , LoopUnrollMin  , MinSurvivorRatio , NewRatio , TieredStopAtLevel , TieredCompilation , AllowParallelDefineClass , AllowVectorizeOnDemand , AlwaysCompileLoopMethods , AlwaysPreTouch , AlwaysTenure , BackgroundCompilation , DoEscapeAnalysis , UseInlineCaches , UseLoopPredicate , UseStringDeduplication , UseSuperWord , UseTypeSpeculation" > ${RESULTS_DIR_ROOT}/Metrics-config.log
+echo ", DEPLOYMENT_NAME , NAMESPACE , IMAGE_NAME , CONTAINER_NAME" > ${RESULTS_DIR_ROOT}/deploy-config.log
+
+echo "INSTANCES , CLUSTER_CPU% , C_CPU_REQ% , C_CPU_LIM% , CLUSTER_MEM% , C_MEM_REQ% , C_MEM_LIM% " > ${RESULTS_DIR_ROOT}/Metrics-cluster.log
+echo "RUN , CPU_REQ , MEM_REQ , CPU_LIM , MEM_LIM , THROUGHPUT , RESPONSETIME , WEB_ERRORS , RESPONSETIME_MAX , RESPONSETIME_STDEV , CPU , CPU_MIN , CPU_MAX , MEM , MEM_MIN , MEM_MAX" > ${RESULTS_DIR_ROOT}/Metrics-raw.log
+echo "INSTANCES ,  MEM_RSS , MEM_USAGE " > ${RESULTS_DIR_ROOT}/Metrics-mem-prom.log
+echo "INSTANCES ,  CPU_USAGE" > ${RESULTS_DIR_ROOT}/Metrics-cpu-prom.log
+echo ", 50p , 95p , 98p , 99p , 99.9p" > ${RESULTS_DIR_ROOT}/Metrics-percentile-prom.log
+echo "ITR , THROUGHPUT , RESPONSE_TIME , THROUGHPUT_RATE_3m , RESPONSE_TIME_RATE_3m" >> ${RESULTS_DIR_ROOT}/server_requests-metrics-measure-raw.log
+echo "THROUGHPUT_RATE_1m , RESPONSE_TIME_RATE_1m , THROUGHPUT_RATE_3m , RESPONSE_TIME_RATE_3m , THROUGHPUT_RATE_5m , RESPONSE_TIME_RATE_5m , THROUGHPUT_RATE_6m , RESPONSE_TIME_RATE_6m " > ${RESULTS_DIR_ROOT}/Metrics-rate-prom.log
+echo "INSTANCES , 50p_HISTO , 95p_HISTO , 97p_HISTO , 99p_HISTO , 99.9p_HISTO , 99.99p_HISTO , 99.999p_HISTO , 100p_HISTO" >> ${RESULTS_DIR_ROOT}/Metrics-quantiles-prom.log
+echo "INSTANCES , CPU_MAXSPIKE , MEM_MAXSPIKE "  >> ${RESULTS_DIR_ROOT}/Metrics-spikes-prom.log
+echo "50p_HISTO , 95p_HISTO , 97p_HISTO , 99p_HISTO , 99.9p_HISTO , 99.99p_HISTO , 99.999p_HISTO , 100p_HISTO" >> ${RESULTS_DIR_ROOT}/Metrics-histogram-prom.log
+
+echo ", ${CPU_REQ} , ${MEM_REQ} , ${CPU_LIM} , ${MEM_LIM} , ${quarkustpcorethreads} , ${quarkustpqueuesize} , ${quarkusdatasourcejdbcminsize} , ${quarkusdatasourcejdbcmaxsize} , ${FreqInlineSize} , ${MaxInlineLevel} , ${MinInliningThreshold} , ${CompileThreshold} , ${CompileThresholdScaling} , ${ConcGCThreads} , ${InlineSmallCode} , ${LoopUnrollLimit} , ${LoopUnrollMin} , ${MinSurvivorRatio} , ${NewRatio} , ${TieredStopAtLevel} , ${TieredCompilation} , ${AllowParallelDefineClass} , ${AllowVectorizeOnDemand} , ${AlwaysCompileLoopMethods} , ${AlwaysPreTouch} , ${AlwaysTenure} , ${BackgroundCompilation} , ${DoEscapeAnalysis} , ${UseInlineCaches} , ${UseLoopPredicate} , ${UseStringDeduplication} , ${UseSuperWord} , ${UseTypeSpeculation} " >> ${RESULTS_DIR_ROOT}/Metrics-config.log
+echo ", renaissance-sample , ${NAMESPACE} , ${renaissance_IMAGE} , renaissance0.14.1" >> ${RESULTS_DIR_ROOT}/deploy-config.log
+if [ ${CLUSTER_TYPE} == "minikube" ]; then
+#	reload_minikube ${K_CPU} ${K_MEM}
+	fwd_prometheus_port_minikube
+fi
+#TODO Create a function on how many DB inst required for a server. For now,defaulting it to 1
+# Scale the instances and run the iterations
+for (( scale=1; scale<=${TOTAL_INST}; scale++ ))
+do
+	RESULTS_SC=${RESULTS_DIR_ROOT}/scale_${scale}
+	echo "Run in progress..."
+	echo "***************************************" >> ${LOGFILE}
+	echo "Run logs are placed at... " ${RESULTS_DIR_ROOT} >> ${LOGFILE}
+	echo "***************************************" >> ${LOGFILE}
+	echo "Running the benchmark with ${scale}  instances with ${TOTAL_ITR} iterations having ${WARMUPS} warmups and ${MEASURES} measurements" >> ${LOGFILE}
+	# Perform warmup and measure runs
+	runIterations ${scale} ${TOTAL_ITR} ${WARMUPS} ${MEASURES} ${RESULTS_SC}
+	echo "Parsing results for ${scale} instances" >> ${LOGFILE}
+	# Parse the results
+	${SCRIPT_REPO}/parsemetrics-wrk.sh ${TOTAL_ITR} ${RESULTS_SC} ${scale} ${WARMUPS} ${MEASURES} ${NAMESPACE} ${SCRIPT_REPO} ${CLUSTER_TYPE} ${APP_NAME}
+	sleep 5
+	${SCRIPT_REPO}/parsemetrics-promql.sh ${TOTAL_ITR} ${RESULTS_SC} ${scale} ${WARMUPS} ${MEASURES} ${SCRIPT_REPO}
+	
+done
+
+## Cleanup all the deployments
+#${SCRIPT_REPO}/tfb-cleanup.sh -c ${CLUSTER_TYPE} -n ${NAMESPACE} >> ${LOGFILE}
+sleep 10
+echo " "
+# Display the Metrics log file
+paste ${RESULTS_DIR_ROOT}/Metrics-prom.log ${RESULTS_DIR_ROOT}/Metrics-wrk.log ${RESULTS_DIR_ROOT}/Metrics-config.log ${RESULTS_DIR_ROOT}/deploy-config.log
+#paste ${RESULTS_DIR_ROOT}/Metrics-quantiles-prom.log
+
+paste ${RESULTS_DIR_ROOT}/Metrics-prom.log ${RESULTS_DIR_ROOT}/Metrics-wrk.log ${RESULTS_DIR_ROOT}/deploy-config.log > ${RESULTS_DIR_ROOT}/output.csv
