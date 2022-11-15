2a9449cd7adce3aebe736bf1c9c2bd6764eadfb9
kusumachalasani <kchalasa@redhat.com>
2022-11-15 14:19:44 +0000

    Update monitoring load
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- techempower/scripts/monitoring_load.sh
+++ techempower/scripts/monitoring_load.sh
@@ -2,28 +2,34 @@ HYPERFOIL_DIR=$1
 RESULTS_LOG=$2
 IP_ADDR=$3
 DURATION=$4
-THREAD=$5
-CONNECTIONS=$6
 
 INTERVAL=900
+CONNECTIONS=1
+THREAD=1
 runtime="${DURATION} seconds"
 endtime=$(date -ud "$runtime" +%s)
 
+if [[ ${INTERVAL} -ge ${DURATION} ]] ; then
+	INTERVAL=${DURATION}
+fi
+
 while [[ $(date -u +%s) -le $endtime ]]
 do
-	if [[ ${INTERVAL} -ge ${DURATION} ]] ; then
-        	INTERVAL=${DURATION}
-	fi
+	
+	cmd="${HYPERFOIL_DIR}/wrk.sh --latency --threads=${THREAD} --connections=${CONNECTIONS} --duration=${INTERVAL}s http://${IP_ADDR}/db"
+        echo "CMD = ${cmd}"
+	${cmd}
+	sleep 10
 
-	for (( connection=1; connection <= 512 ; connection++))
-	do
-		for (( thread=1 ; thread <= 56 ; thread++ ))
-		do
-			cmd="${HYPERFOIL_DIR}/wrk.sh --latency --threads=${THREAD} --connections=${CONNECTIONS} --duration=${INTERVAL}s http://${IP_ADDR}/db"
-			echo "CMD = ${cmd}"
-			${cmd}
-			sleep 10
-		done
-	done
-	sleep 300
+	THREAD=$((THREAD+1))
+	if [[ ${THREAD} == 56 ]]; then
+		THREAD=1
+		CONNECTIONS=$((CONNECTIONS+4))
+		sleep 120
+	fi
+	if [[ ${CONNECTIONS} == 512 ]]; then
+		CONNECTIONS=1
+		THREAD=1
+		sleep 200
+	fi
 done
