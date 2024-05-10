bebe7e01e9ae772a274ce4610bc56a47337ae0a9
kusumachalasani <kchalasa@redhat.com>
2024-05-10 13:16:24 +0530

    Format changes
    Signed-off-by: kusumachalasani <kchalasa@redhat.com>

--- techempower/docker/tfb-load/run_hyperfoil_load.sh
+++ techempower/docker/tfb-load/run_hyperfoil_load.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+#
+# Copyright (c) 2020, 2021 IBM Corporation, RedHat and others.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+### Script to run workload for TFB benchmark###
 
 # Set default values for parameters if not provided
 IP_ADDR="${1:-localhost:8080}"
