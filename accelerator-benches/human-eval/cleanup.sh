bad7ce58e40ee61683b3d269bbf5cd212bcc6b0d
bhanvimenghani <menghanibhanvi@gmail.com>
2024-12-09 16:19:58 +0530

    change nsp in pvc yaml too

--- accelerator-benches/human-eval/cleanup.sh
+++ accelerator-benches/human-eval/cleanup.sh
@@ -3,6 +3,7 @@
 JOB_NAME=human-eval-deployment-job
 PVC_NAME=human-eval-pvc
 JOB_YAML=./manifests/job.yaml
+PVC_YAML=./manifests/pvc.yaml
 
 if [ -z "$1" ]; then
   echo "Error: Provide namespace to uninstall the job."
@@ -13,6 +14,9 @@ NAMESPACE=$1
 
 # reset the job yaml
 cp ${JOB_YAML}.bak $JOB_YAML
+
+# reset pvc yaml
+cp ${PVC_YAML}.bak $PVC_YAML
 
 #  delete job
 oc delete job $JOB_NAME -n "$NAMESPACE"
